01fe5b383f6f69c3cfb44da8d94ee23ca0eb695b
Gabriel Dornelas <zerick.gd@gmail.com>
2025-02-04 01:17:01 -0300

    refactor: remove unnecessary files

--- flaskr/views/user_view.py
+++
@@ -1,7 +0,0 @@
-def user_response(user):
-    """Formata um usuário para resposta"""
-    return {
-        "id": str(user._id),
-        "username": user.username,
-        "email": user.email
-    }
